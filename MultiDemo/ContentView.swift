bacc869d264d7d0b465a52a1413095a2e4b54738
CGMajoor <cgmajoor@gmail.com>
2023-03-30 14:59:55 +0200

    add speed setting slider as environment object

--- MultiDemo/ContentView.swift
+++ MultiDemo/ContentView.swift
@@ -8,54 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     @StateObject var demoData : DemoData = DemoData()
-
+    let speedsetting = SpeedSetting()
+    
     let myHStack = HStack {
         Text("MyHStack")
         Image(systemName: "globe")
     }
-
+    
     struct WifiImageView: View {
         @Binding var wifiEnabled : Bool
         var body: some View {
             Image(systemName: wifiEnabled ? "wifi" : "wifi.slash")
         }
     }
-
+    
     var body: some View {
         VStack {
-            Toggle(isOn: $demoData.wifiEnabled) {
-                Text("Enable Wi-Fi")
-            }
-            WifiImageView(wifiEnabled: $demoData.wifiEnabled)
-            TextField("Enter user name", text: $demoData.currentUser)
-                .modifier(StandardTitle())
-            Text("Hello \(demoData.currentUser)")
-            Text("\(demoData.currentUser), you are user number \(demoData.userCount)")
-            myHStack
-            Text("sample")
-                .padding()
-                .border(.green)
-            Label(
-                title: {
-                    Text("Label with Icon")
-                        .font(.largeTitle)
-                },
-                icon: {
-                    Circle()
-                        .fill(.blue)
-                        .frame(width: 25, height: 25)
+            Group {
+                SpeedControlView()
+                SpeedDisplayView()
+                Toggle(isOn: $demoData.wifiEnabled) {
+                    Text("Enable Wi-Fi")
                 }
-            )
-            Button( action: buttonPressed ) {
-                Image(systemName: "square.and.arrow.down")
+                WifiImageView(wifiEnabled: $demoData.wifiEnabled)
+                TextField("Enter user name", text: $demoData.currentUser)
+                    .modifier(StandardTitle())
+                Text("Hello \(demoData.currentUser)")
+                Text("\(demoData.currentUser), you are user number \(demoData.userCount)")
+                myHStack
+                Text("sample")
+                    .padding()
+                    .border(.green)
             }
-        }
+            Group {
+                Label(
+                    title: {
+                        Text("Label with Icon")
+                            .font(.largeTitle)
+                    },
+                    icon: {
+                        Circle()
+                            .fill(.blue)
+                            .frame(width: 25, height: 25)
+                    }
+                )
+                Button( action: buttonPressed ) {
+                    Image(systemName: "square.and.arrow.down")
+                }
+            }
+        }.environmentObject(speedsetting)
     }
-
+    
     func buttonPressed() {
         print("Button pressed")
+    }
+}
+
+struct SpeedControlView: View {
+    @EnvironmentObject var speedsetting: SpeedSetting
+    var body: some View {
+        Slider(value: $speedsetting.speed, in: 0...100)
+    }
+}
+
+struct SpeedDisplayView: View {
+    @EnvironmentObject var speedsetting: SpeedSetting
+    var body: some View {
+        Text("Speed = \(speedsetting.speed)")
     }
 }
 
---
+++ MultiDemo/SpeedSetting.swift
@@ -0,0 +1,15 @@
+//
+//  SpeedSetting.swift
+//  MultiDemo
+//
+//  Created by Ceren Gazioglu Majoor on 30/03/2023.
+//
+
+import Foundation
+import Combine
+
+class SpeedSetting: ObservableObject {
+    @Published var speed = 0.0
+
+   
+}
