f2d71772c1507753ab47800131f3059475103b5f
CGMajoor <cgmajoor@gmail.com>
2023-03-30 16:16:42 +0200

    add scenePhases

--- MultiDemo/MultiDemoApp.swift
+++ MultiDemo/MultiDemoApp.swift
@@ -9,9 +9,24 @@ import SwiftUI
 
 @main
 struct MultiDemoApp: App {
+
+    @Environment(\.scenePhase) private var scenePhase
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .onChange(of: scenePhase, perform: { phase in
+            switch phase {
+            case .active:
+                print("Active")
+            case .inactive:
+                print("Inactive")
+            case .background:
+                print("Background")
+            default:
+                print("Unknown scenephase")
+            }
+        })
     }
 }
