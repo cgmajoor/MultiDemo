90cd2d817b80bac16c6a130d6ec6a123c91330fd
CGMajoor <cgmajoor@gmail.com>
2023-03-30 10:09:34 +0200

    Initial Commit

---
+++ MultiDemo/MultiDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiDemoApp.swift
+//  MultiDemo
+//
+//  Created by Ceren Gazioglu Majoor on 30/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MultiDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
